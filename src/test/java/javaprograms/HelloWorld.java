78b92edae4fa4d72223c91e765ae990634d0d167
mahesh.pund <devops.maheshpund@gmail.com>
2024-04-28 10:40:49 +0530

    program1

--- src/test/java/javaprograms/HelloWorld.java
+++ src/test/java/javaprograms/HelloWorld.java
@@ -1,7 +1,10 @@
 package javaprograms;
 
+import org.testng.annotations.Test;
+
 public class HelloWorld {
-	public static void main(String args[]) {
+	@Test
+	public void helloWorldMethod() {
 		System.out.println("Hello World ...!");
 	}
 }
